b26667e226c8f64d12b2b96368448193c5c8df30
pengpa0012 <peng.ilikha@gmail.com>
2021-05-27 22:09:38 +0800

    remove console log

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,33 +1,32 @@
-import colors from 'vuetify/es5/util/colors'
+import colors from "vuetify/es5/util/colors";
 
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - Book-API',
-    title: 'Book-API',
+    title: "Book API",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Search book using Google Books API"
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -35,18 +34,17 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
+    "@nuxtjs/vuetify"
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ["~/assets/variables.scss"],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
@@ -62,6 +60,5 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {}
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,74 @@
 <template>
-  <div>
-    <input type="search" v-model="inputValue" />
-    <button @click="searchBook">Search</button>
-  </div>
+  <v-app id="inspire">
+    <v-app-bar app shrink-on-scroll class="d-flex justify-center text-center">
+      <v-toolbar-title class="display-1 pt-5"
+        ><span class="subtitle-1">Made by Godfrey Necesario</span><br />
+        Google Book API</v-toolbar-title
+      >
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <v-row class="pt-10 px-4">
+          <v-text-field
+            v-model="inputValue"
+            solo
+            label="Search for books"
+            clearable
+            :append-outer-icon="'mdi-book-search'"
+            @click:append-outer="searchBook"
+          ></v-text-field>
+        </v-row>
+        <v-row cols="3" class="d-flex justify-center py-5">
+          <v-card
+            v-for="(book, index) in allBooks"
+            :key="index"
+            class="mx-2 my-3 align-start"
+            max-width="300"
+            min-width="300"
+          >
+            <v-img
+              class="white--text align-end"
+              height="300px"
+              :src="book.volumeInfo.imageLinks.thumbnail"
+              alt="poster"
+            ></v-img>
+            <div class="d-flex flex-column align-stretch">
+              <v-card-title class="subtitle-2 pb-0 text-truncate">
+                <div class="text-truncate">
+                  {{ book.volumeInfo.title }}
+                </div>
+              </v-card-title>
+              <v-card-text class="text--primary">
+                <div class="caption">
+                  Publisher:
+                  {{
+                    book.volumeInfo.publisher
+                      ? book.volumeInfo.publisher
+                      : "Unknown"
+                  }}
+                </div>
+
+                <div class="text-truncate">
+                  {{
+                    book.volumeInfo.description
+                      ? book.volumeInfo.description
+                      : "No description attached"
+                  }}
+                </div>
+              </v-card-text>
+              <v-card-actions>
+                <a :href="book.volumeInfo.infoLink" target="_blank">
+                  <v-btn color="orange" text>
+                    Visit Book
+                  </v-btn>
+                </a>
+              </v-card-actions>
+            </div>
+          </v-card>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
@@ -10,20 +76,56 @@ export default {
   components: {},
   data() {
     return {
-      inputValue: ""
+      inputValue: "",
+      allBooks: []
     };
   },
   methods: {
     searchBook() {
+      if (this.inputValue == "") {
+        alert("Type something");
+        return;
+      }
+      this.allBooks = [];
+
       const key = "AIzaSyBOL4MIwcR42oZwTaRvN6BXjIB1Memp4zo";
 
       fetch(
         `https://www.googleapis.com/books/v1/volumes?q=${this.inputValue}&key=${key}`
       )
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+          if (data.totalItems <= 0) {
+            alert("try other keywords");
+            this.inputValue = "";
+            return;
+          }
+          let allVerifiedBooks = data.items.filter(el => {
+            return el.volumeInfo.imageLinks;
+          });
+          allVerifiedBooks.forEach(el => {
+            this.allBooks.push(el);
+          });
+        })
         .catch(err => console.log(err));
     }
+  },
+  created() {
+    const key = "AIzaSyBOL4MIwcR42oZwTaRvN6BXjIB1Memp4zo";
+
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=javascript&key=${key}`)
+      .then(res => res.json())
+      .then(data => {
+        if (data.totalItems <= 0) {
+          alert("try other keywords");
+          this.inputValue = "";
+          return;
+        }
+        data.items.forEach(el => {
+          this.allBooks.push(el);
+        });
+      })
+      .catch(err => console.log(err));
   }
 };
 </script>
