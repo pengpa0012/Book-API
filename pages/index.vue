a7919bac72081b623a6c718bb0ae8c1b634e6564
pengpa0012 <peng.ilikha@gmail.com>
2021-05-27 22:07:06 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input type="search" v-model="inputValue" />
+    <button @click="searchBook">Search</button>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      inputValue: ""
+    };
+  },
+  methods: {
+    searchBook() {
+      const key = "AIzaSyBOL4MIwcR42oZwTaRvN6BXjIB1Memp4zo";
+
+      fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=${this.inputValue}&key=${key}`
+      )
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
+    }
+  }
+};
+</script>
